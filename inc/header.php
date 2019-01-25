38ad24ae0ba47777d386c59468e78e494201fee8
Kimberly Praxel <kimberly.praxel@gmail.com>
2019-01-25 00:07:16 -0800

    added data

--- inc/header.php
+++ inc/header.php
@@ -5,3 +5,38 @@
  * Date: 2019-01-23
  * Time: 17:55
  */
+?>
+
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <!--============== basic meta data ==============-->
+    <meta charset="UTF-8">
+    <title>Memory Book Project</title>
+    <meta name="robots" content="index, follow"/>
+    <meta name="geo.country" content="USA" />
+    <meta name="geo.region" content="US-WA" />
+    <meta name="geo.placename" content="Spokane Valley, WA" />
+    <!--============== css ==============-->
+
+
+
+    <!--============== javascript ==============-->
+
+
+    <!--============== analytics ==============-->
+
+
+</head>
+<body>
+<header>
+
+</header>
+
+</body>
+
+
+
+
+
+</html>
--- index.php
+++ index.php
@@ -5,3 +5,7 @@
  * Date: 2019-01-23
  * Time: 17:52
  */
+
+  require_once('./inc/header.php'); ?>
+<?php $page_name = "Home"; ?>
+
